bc2a9b9d9f7e7d358f4b5d2bdfe0976b4af7efd7
Judzhin Miles <info@msbios.com>
2021-03-10 12:10:40 +0200

    CertBot

--- .docker/nginx/init-letsencrypt.v1.sh
+++ .docker/nginx/init-letsencrypt.v1.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="erc-distribution.ge" "www.erc-distribution.ge"
+domains=(erc-distribution.ge www.erc-distribution.ge)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="msbios@gmail.com" # Adding a valid address is strongly recommended
