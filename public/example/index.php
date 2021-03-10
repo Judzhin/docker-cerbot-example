385edf0a0c7b4f9981151d26db9b93ed488b3fba
Judzhin Miles <info@msbios.com>
2021-03-10 16:10:32 +0200

    CertBot

--- public/example/index.php
+++ public/example/index.php
@@ -5,4 +5,6 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 echo \Example\Application::run();
 echo "<br>";
-printf("Domain: %s, at time %s", $_SERVER['SERVER_NAME'], date('Y.m.d:H:i:s'));
+printf("Directory: %s, File: %s", __DIR__, __FILE__);
+echo "<br>";
+printf("Domain: %s, At time %s", $_SERVER['SERVER_NAME'], date('Y.m.d:H:i:s'));
